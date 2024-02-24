5915bea9899d0ee5faa14636715779b4ef8997b8
Bechlor <abhisinghtomar2004@gmail.com>
2024-02-24 15:31:36 +0530

    first website hosting

---
+++ index.js
@@ -0,0 +1,5 @@
+const showMore= documnent.querySelectorAll(".show-more");
+const option = documnent.querySelectorAll(".option");
+showMore.addEventlistener("click",(e) => {
+  option.classList.toggle(console.log("hello"))
+});
